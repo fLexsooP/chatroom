cbd83c12d03399f2ec034006c862ce4eb25983bc
Chence Shi <ascc594557067@gmail.com>
2023-06-10 02:14:40 -0700

    add base template

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ from flask_socketio import join_room, leave_room, SocketIO, send
 import random
 from string import ascii_uppercase
 
-app =  Flask(__name__)
+app = Flask(__name__)
 app.config["SECRET_KEY"] = "27eR0/iGMMojTBKCGeNQp7O4zZ6pyG13pAWub4bhjK8="
 socketio = SocketIO(app)
 
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return render_template("home.html")
+
+
 if __name__ == "__main__":
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=True, host='127.0.0.1', port='8000')
